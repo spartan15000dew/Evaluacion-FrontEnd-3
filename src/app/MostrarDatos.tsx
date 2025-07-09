3c6b372bd1cad5ff556d6f50a32209a4e4018e7a
spartan15000dew <spartan15000dew@gmail.com>
2025-07-08 20:18:01 -0400

    feat: añadir funcionalidad de edición de elementos en la lista
    Se ha implementado la funcionalidad para editar elementos de la lista. Los cambios se sincronizan con localStorage y con el estado de la lista de registros.

--- src/app/MostrarDatos.tsx
+++ src/app/MostrarDatos.tsx
@@ -23,12 +23,18 @@ export const MostrarDatos = ({ eventos, setEventos, formulario, setFormulario } 
     setEventos(prevEventos =>
       prevEventos.filter(evento => evento.id !== eventoId)
     )
+  }
 
+  function handleChange(id: number, value: string) {
+    const eventosActualizados = eventos.map(evento =>
+      evento.id === id ? { ...evento, "nombre": value } : evento
+    )
+    setEventos(eventosActualizados)
   }
   
   let elementosEventos = eventos.map((evento, i) =>
     <li key={i}>
-      <input type="text" value={evento.nombre}/>
+      <input type="text" value={evento.nombre} onChange={(e) => handleChange(evento.id, e.target.value)}/>
       <button onClick={() => eliminarDeLaLista(evento.id)}>Borrar</button>
     </li>
   )
